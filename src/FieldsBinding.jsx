8d782154e7ce9570f689a76b947b71957ec15a36
Albert Gao <albertgaohy@gmail.com>
2017-10-16 23:29:44 +1300

    Update the variable name collision

--- src/FieldsBinding.jsx
+++ src/FieldsBinding.jsx
@@ -8,16 +8,14 @@ export default class FieldsBinding extends React.Component {
     lib.startValidating( target, schema, formStatus, update )
   }
 
-  handleOnChange = e => {
-    return this.validate(e.target);
-  };
+  handleOnChange = e =>  (this.validate(e.target));
 
   render() {
     const names = Object.keys(this.props.schema);
-    const child = React.Children.map(this.props.children, child => {
+    const { fields } = this.props.formStatus;    
+    const newChild = React.Children.map(this.props.children, child => {
       const childName = child.props.name;
       if (names.includes(childName)) {
-        const { fields } = this.props.formStatus;
         return React.cloneElement(child, {
           status: fields[childName].status,
           hint: fields[childName].errorText,
@@ -26,12 +24,13 @@ export default class FieldsBinding extends React.Component {
       }
       return child;
     });
-    return <section onChange={this.handleOnChange}>{child}</section>;
+    return <section onChange={this.handleOnChange}>{newChild}</section>;
   }
 }
 
 FieldsBinding.propTypes = {
   schema: PropTypes.object.isRequired,
   formStatus: PropTypes.object.isRequired,
-  update: PropTypes.func.isRequired
+  update: PropTypes.func.isRequired,
+  children: PropTypes.any.isRequired
 };
