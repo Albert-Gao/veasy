898e2d7429d691de66ae7ba54928e4e48a252af9
Albert Gao <albertgaohy@gmail.com>
2017-10-17 23:09:50 +1300

    #4 Add a test case, which the user try to add their custom key into the state under the same name which used in the schema

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -77,4 +77,48 @@ describe('Test the createInitialState method', () => {
       C: 1
     });
   });
+
+  test('Should return an object with user`s state', () => {
+    schema.title.default = 'I am default';
+    schema.name.default = 'albert';
+    const userState = {
+      name: {
+        firstName: 'Albert',
+        lastName: 'Gao'
+      },
+      title: {
+        realLength: 123
+      },
+      A: 'super',
+      B: {
+        a: 1,
+        b: 'a'
+      },
+      C: 1
+    };
+    const state = new EasyV(component, schema).createInitialState(userState);
+    expect(state).toEqual({
+      isFormOK: false,
+      name: {
+        status: 'normal',
+        errorText: '',
+        value: 'albert',
+        firstName: 'Albert',
+        lastName: 'Gao'
+      },
+      title: {
+        status: 'normal',
+        errorText: '',
+        value: 'I am default',
+        realLength: 123
+      },
+      A: 'super',
+      B: {
+        a: 1,
+        b: 'a'
+      },
+      C: 1
+    });
+  });
+
 });
