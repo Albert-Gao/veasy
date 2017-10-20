10d8b9ab47080f393e927a4c9f3134be759ce0aa
Albert Gao <albertgaohy@gmail.com>
2017-10-21 03:14:24 +1300

    Remove useless code

--- src/EasyV.jsx
+++ src/EasyV.jsx
@@ -5,33 +5,16 @@ import * as lib from './helpers';
 import is from 'is_js';
 
 export default class EasyV extends React.Component {
-  getChildren = () => {
-    const { schema, allState, children } = this.props;
-    const names = Object.keys(schema);
-    return React.Children.map(children, child => {
-      const childName = child.props.name;
-      if (names.includes(childName)) {
-        return React.cloneElement(child, {
-          status: allState[childName].status,
-          hint: allState[childName].errorText,
-          value: allState[childName].value
-        });
-      }
-      return child;
-    });
-  };
-
   handleOnChange = e => {
     e.preventDefault();
     const { schema, allState, update } = this.props;
     lib.startValidating(e.target, schema, update, allState);
   };
 
-  isRegisteredComponent = child => {
+  isRegisteredComponent = (child, childName) => {
     if (is.lowerCase(child.type[0])) return false;
 
     const names = Object.keys(this.props.schema);
-    const childName = child.props.name;
     if (React.isValidElement(child) && names.includes(childName)) {
       return true;
     }
@@ -39,18 +22,18 @@ export default class EasyV extends React.Component {
   };
 
   cloneElement = (child, childName) => {
-    const { allState } = this.props;
+    const childProp = this.props.allState[childName];
     return React.cloneElement(child, {
-      status: allState[childName].status,
-      errorText: allState[childName].errorText,
-      value: allState[childName].value
+      status: childProp.status,
+      errorText: childProp.errorText,
+      value: childProp.value
     });
   };
 
   recursiveCloneChildren = children => {
     return React.Children.map(children, child => {
-      if (this.isRegisteredComponent(child)) {
-        const childName = child.props.name;
+      const childName = child.props.name;      
+      if (this.isRegisteredComponent(child, childName)) {
         return this.cloneElement(child, childName);
       }
       
