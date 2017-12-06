9542d96415e7038701cba09dd92842a823828c42
Albert Gao <albertgaohy@gmail.com>
2017-12-06 20:33:54 +0800

    #17 Optimize the test case description

--- tests/integration/Veasy.intergration.test.js
+++ tests/integration/Veasy.intergration.test.js
@@ -1,9 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies,react/jsx-filename-extension */
 import Enzyme, { mount, shallow, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import toJson from 'enzyme-to-json';
 import React from 'react';
-import renderer from 'react-test-renderer';
 import MyForm from './components/MyForm';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -12,8 +10,8 @@ describe('Test <MyForm>', () => {
   beforeEach(() => {
   });
 
-  test('should render a component with submit button not disable', () => {
-    const wrapper = mount( <MyForm /> );
+  test('should render a component with state.isFormOK equals true', () => {
+    const wrapper = shallow( <MyForm /> );
     expect(wrapper.state().isFormOK).toEqual(true);
   });
 });
